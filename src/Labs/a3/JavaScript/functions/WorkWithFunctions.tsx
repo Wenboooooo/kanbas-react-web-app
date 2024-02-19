6ebbcaeb781513668de74ad70a79b5447817d537
Wenbo Liu <liu.wenbo@northeastern.edu>
2024-02-18 23:51:36 -0800

    JavaScript and React.js Assignment 3

---
+++ src/Labs/a3/JavaScript/functions/WorkWithFunctions.tsx
@@ -0,0 +1,27 @@
+import ArrowFunctions from "./ArrowFunctions";
+import ES5Functions from "./ES5Functions";
+import ImpliedReturn from "./ImpliedReturn";
+import FunctionParenthesisAndParameters from "./FunctionParenthesisAndParameters";
+import WorkingWithArrays from "../arrays/WorkingWithArrays";
+import ArrayIndexAndLength from "../arrays/ArrayIndexAndLength";
+import AddingAndRemovingDataToFromArrays from "../arrays/AddingAndRemovingDataToFromArrays";
+import MapFunction from "../arrays/MapFunction";
+
+
+
+
+
+function WorkWithFunctions() {
+    return (
+        <div>
+            <ES5Functions/>
+            <ArrowFunctions/>
+            <ImpliedReturn/>
+            <FunctionParenthesisAndParameters/>
+
+        </div>
+    );
+
+}
+
+export  default WorkWithFunctions;
